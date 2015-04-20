67dd8d953ceffd7cbcdd790ff39ffdaf6a22d745
Andrew Poland <andrew.poland@angieslist.com>
2015-04-20 16:57:43 -0400

    calc

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,7 +11,10 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    
+    val stuff = Var(24.0)
+    val map = Map(("a", Signal(24.0)))
+    map
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
